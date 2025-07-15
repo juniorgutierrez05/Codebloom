4f3ab0b4b4f673dc5da2e00cd36ab16c307a4202
juniorgutierrez05 <junior.gutierrezm05@gmail.com>
2025-07-14 21:47:58 -0500

    actualizacion componente hero y altura de footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,9 +11,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-      <body className="bg-white text-gray-900">
+      <body>
         <Navbar/>
-        <main className="min-h-screen">{children}</main>
+        <main>{children}</main>
         <Footer/>
       </body>
     </html>
--- src/components/Hero/hero.jsx
+++ src/components/Hero/hero.jsx
@@ -1,26 +1,33 @@
-import styles from "./hero.module.css"
-export default function hero(){
-    return(
-        <div>
-            <img className={styles.fondo} src="/Image/image.png" alt="Imagen de intro" />
-            <div className={styles.content}>
-                <h1 className={styles.eslogan}>Crecemos ideas con codigo</h1>
-                <p className={styles.textIntro}>Tienes una idea, nosotros el <br/>
-                código para hacerla crecer.</p>
-            <p className={styles.descripcion}>En Codebloom trabajamos contigo para crear soluciones digitales a la<br/>
-             medida de tus sueños y necesidades. Desde páginas web hasta sistemas<br/>
-             completos, combinamos diseño, tecnología e innovación para impulsar tu<br/>
-              marca, optimizar tus procesos y llevar tu negocio al siguiente nivel.<br/>
-              Creamos herramientas que funcionan, comunican y crecen contigo.</p>
-            </div>
-            <div className={styles.divButtons}>
-                <div className={styles.buttonOne}>
-                    <h2 className={styles.title}>Mas informacion</h2>
-                </div>
-                <div className={styles.buttonTwo}>
-                    <h2 className={styles.title}>Cotizar</h2>
-                </div>
-            </div>
+import styles from "./hero.module.css";
+
+export default function Hero() {
+  return (
+    <section className={styles.hero}>
+      <img className={styles.fondo} src="/Image/image.png" alt="Imagen de intro" />
+
+      <div className={styles.content}>
+        <h1 className={styles.eslogan}>Crecemos ideas con código</h1>
+        <p className={styles.textIntro}>
+          Tienes una idea, nosotros el <br />
+          código para hacerla crecer.
+        </p>
+        <p className={styles.descripcion}>
+          En Codebloom trabajamos contigo para crear soluciones digitales a la <br />
+          medida de tus sueños y necesidades. Desde páginas web hasta sistemas <br />
+          completos, combinamos diseño, tecnología e innovación para impulsar tu <br />
+          marca, optimizar tus procesos y llevar tu negocio al siguiente nivel. <br />
+          Creamos herramientas que funcionan, comunican y crecen contigo.
+        </p>
+      </div>
+
+      <div className={styles.divButtons}>
+        <div className={styles.buttonOne}>
+          <h2 className={styles.title}>Más información</h2>
         </div>
-    )
+        <div className={styles.buttonTwo}>
+          <h2 className={styles.title}>Cotizar</h2>
+        </div>
+      </div>
+    </section>
+  );
 }
